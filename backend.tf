6f88d95e91502b9c2465377e01bb4ab602817d69
thiagodecastro <41386429+thiagodecastro@users.noreply.github.com>
2022-02-10 22:16:08 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,8 +4,6 @@ terraform {
    bucket         = "test-s3-state"
    key            = "state/terraform.tfstate"
    region         = "ca-central-1"
-  #  encrypt        = true
-  #  kms_key_id     = "alias/terraform-bucket-key"
-  #  dynamodb_table = "terraform-state"
+  
  }
 }
