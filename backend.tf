157396281c7340ceee70bc4fc9cbfea8589108cf
Thiago de Castro Sousa <thiago@rarolabs.com.br>
2022-02-11 00:17:21 -0300

    us

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
 
  backend "s3" {
-   bucket         = "s3-state-assess-test"
+   bucket         = "s3-state-assessment-test"
    key            = "state/terraform.tfstate"
    region         = "us-east-1"
   
